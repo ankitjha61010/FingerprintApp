0cd265d42be874da45bcf89f540fe532764a6b0a
ankitjha61010 <121237027+ankitjha61010@users.noreply.github.com>
2024-01-21 00:17:08 +0530

    ttt
    ttt

--- App.js
+++ App.js
@@ -1,95 +1,42 @@
-import {View, Text, TextInput, Button, Alert} from 'react-native';
-import React, {useState, useEffect} from 'react';
-import * as Keychain from 'react-native-keychain';
-import TouchID from 'react-native-touch-id';
+import {View, Text} from 'react-native';
+import React, {useEffect} from 'react';
+import ReactNativeBiometrics, {
+  BiometryTypes,
+} from 'react-native-face-fingerprint-auth';
+import Fingerprint from 'cordova-plugin-fingerprint-aio'
 const App = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [authenticated, setAuthenticated] = useState(false);
-  const [EnableFingerPrint, setEnableFingerPrint] = useState(false);
+  const rnFaceFingerprintAuth = new ReactNativeBiometrics();
 
   useEffect(() => {
-    authenticateWithTouchID();
+    face();
   }, []);
+  const face = () => {
 
-  async function authenticateWithTouchID() {
-    const optionalConfigObject = {
-      title: 'Well Kept ', // Android
-      imageColor: '#e00606', // Android
-      imageErrorColor: '#ff0000', // Android
-      sensorDescription: 'Touch sensor', // Android
-      sensorErrorDescription: 'Failed', // Android
-      cancelText: 'Cancel', // Android
-      fallbackLabel: 'Show Passcode', // iOS (if empty, then label is hidden)
-      unifiedErrors: false, // use unified error messages (default false)
-      passcodeFallback: false, // iOS - allows the device to fall back to using the passcode, if faceid/touch is not available. this does not mean that if touchid/faceid fails the first few times it will revert to passcode, rather that if the former are not enrolled, then it will use the passcode.
-    };
 
-    try {
-      TouchID.isSupported(optionalConfigObject)
-        .then(biometryType => {
-          // Success code
-          if (biometryType === 'FaceID') {
-            console.log('FaceID is supported.');
-          } else {
-            setEnableFingerPrint(true);
 
-            // Alert.alert('TouchID is supported.');
-          }
-        })
-        .catch(error => {
-          // Failure code
-          console.log(error);
-          setEnableFingerPrint(false);
-        });
-    } catch (error) {
-      console.log(error);
+    Fingerprint.registerBiometricSecret({
+      description: "Some biometric description",
+      secret: "my-super-secret",
+      invalidateOnEnrollment: true,
+      disableBackup: true, // always disabled on Android
+    }, successCallback, errorCallback);
+
+    function successCallback(){
+      alert("Authentication successful");
     }
 
-    try {
-      const authenticated = await TouchID.authenticate(
-        'Authenticate to log in',
-        optionalConfigObject,
-      );
-      console.log('Authenticated with Touch ID:', authenticated);
-      setAuthenticated(authenticated);
-      setEnableFingerPrint(true)
-
-    } catch (error) {
-      console.log("Couldn't authenticate with Touch ID:", error);
-      setEnableFingerPrint(false);
+    function errorCallback(error){
+      alert("Authentication invalid " + error.message);
     }
-  }
 
-  // async function handleLogin() {
-  //   if (authenticated) {
-  //     // User is authenticated with Touch ID, skip login
-  //     console.log('User is authenticated with Touch ID!');
-  //   } else {
-  //     // User is not authenticated with Touch ID, check credentials
-  //     const credentials = await retrieveCredentials();
-  //     if (credentials && credentials.username === username && credentials.password === password) {
-  //       console.log('Login successful!');
-  //       await storeCredentials(username, password);
-  //     } else {
-  //       console.log('Login failed!');
-  //     }
-  //   }
-  // }
 
+    
+  };
   return (
     <View>
-      {/* <TextInput placeholder="Username" value={username} onChangeText={setUsername} />
-      <TextInput placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry /> */}
-      {/* <Button title="Log in" onPress={handleLogin} /> */}
-      {authenticated ? (
-        <Text>Authenticated with Touch ID!</Text>
-      ) : (
-        EnableFingerPrint && (
-          <Button title="Authenticate with Touch ID" onPress={{}} />
-        )
-      )}
+      <Text>App</Text>
     </View>
   );
 };
+
 export default App;
--- keychainService.js
+++ keychainService.js
@@ -1,15 +1,77 @@
-import * as Keychain from 'react-native-keychain';
-import { Platform } from "react-native";
-export const setCredentials = async (username, password) => {
-   return new Promise((resolve, reject) => {
-       // Store the credentials
-       Keychain.setGenericPassword(username, password)
-           .then(resp => {
-               resolve(true)
-           })
-           .catch(err => {
-               console.log("err: ", err);
-               reject(err);
-           });
-   });
-}
+import {View, Text, Button, Switch} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import ReactNativeBiometrics, {BiometryTypes} from 'react-native-biometrics';
+
+const App = () => {
+  const [publicKeyToServer, sendPublicKeyToServer] = useState();
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+  const rnBiometrics = new ReactNativeBiometrics();
+
+  useEffect(() => {
+    Biometric();
+    rnBiometrics.createKeys().then(resultObject => {
+      const {publicKey} = resultObject;
+      console.log(publicKey);
+      sendPublicKeyToServer(publicKey);
+    });
+    isEnabled && createSignatures();
+  }, [isEnabled]);
+
+  const Biometric = async () => {
+    rnBiometrics.isSensorAvailable().then(resultObject => {
+      const {available, biometryType} = resultObject;
+
+      if (available && biometryType === BiometryTypes.TouchID) {
+        console.log('TouchID is supported');
+      }  if (available && biometryType === BiometryTypes.FaceID) {
+        console.log('FaceID is supported');
+      }  if (available && biometryType === BiometryTypes.Biometrics) {
+        console.log('Biometrics is supported');
+      } else {
+        console.log('Biometrics not supported', );
+      }
+    });
+  };
+
+  const createSignatures = () => {
+    let epochTimeSeconds = Math.round(new Date().getTime() / 1000).toString();
+    let payload = epochTimeSeconds + 'Use your FingerPrint';
+
+
+
+
+
+
+    rnBiometrics
+      .createSignature({
+        promptMessage: "Verify it's you",
+        payload: 'Hello, world!',
+        cancelButtonText: 'cancel',
+      })
+      .then(resultObject => {
+        const {success, signature} = resultObject;
+
+        if (success) {
+          console.log(success);
+          console.log(signature);
+          setIsEnabled(true);
+          // verifySignatureWithServer(signature, payload);
+        }
+      });
+  };
+  return (
+    <View>
+      <Switch
+        trackColor={{false: '#767577', true: '#81b0ff'}}
+        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+      <Text>Abhishek is a good Boy</Text>
+    </View>
+  );
+};
+
+export default App;
